e26bf2b970368957fef38ae4de478af81deed7c3
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-20 09:40:07 +0100

    eventlistener create 100 div with number

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -20,32 +20,21 @@ con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81,
 divise in 9 caselle per 9 righe;
 con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, 
 divise in 7 caselle per 7 righe;
+
 */
 
 /* strumenti
 
 */
-
-//selezionare un nodo della DOM nel quale mettere le 100 celle
 const containerEl = document.querySelector('.container')
-//ciclo n volte pre generare n celle 
-let maxCellNumb = 100
-//ciclo n volte pre generare n celle 
-for (let i = 0; i < maxCellNumb; i++) {
-    //per ciascuna iterazione devo generare il markup per ogni singola cella
-    const cellMarkup = `<div class="cell text_center"></div>`;
-    containerEl.innerHTML += cellMarkup
-
-}
-
-const cells = document.querySelectorAll('.cell')
-console.log(cells);
-
-for (let i = 0; i < cells.length; i++) {
-   const thisCell = cells[i]
-   console.log(thisCell)
-   thisCell.addEventListener('click', function () {
-
-    this.classList.toggle('bg_light_blue')
-   }
-)}
+//L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+//Ogni cella ha un numero progressivo, da 1 a 100.
+//Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+button.addEventListener('click', function () {
+    for (let i = 1; i <= 100; i++) {
+        const divEl =document.createElement('div')
+            console.log(i)
+            divEl.append(i)
+            containerEl.append(divEl)
+        }
+})
